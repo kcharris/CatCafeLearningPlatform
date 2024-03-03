0642acf1ddcad0281b0d192435582ee1b7923f0b
kevin harris <kcharris77@gmail.com>
2024-03-03 06:28:12 -0600

    Add upload file button to upload page

--- cclp/src/components/upload.tsx
+++ cclp/src/components/upload.tsx
@@ -1,10 +1,39 @@
-function upload(){
-    
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+function UploadFile(){
+    let res = (
+        <div className="uploadFile">
+            <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                className="uploadButton"
+            >
+                Upload file
+                <VisuallyHiddenInput type="file" />
+            </Button>
+        </div>
+    )
+    return res
 }
 
 export default function Upload(){
     let res = <div className = "upload-wrapper">
-
+        <UploadFile/>
     </div>
     return res
 }
