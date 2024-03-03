dbbc77d194bce88d201b9cf2c113830814a6022a
Saroz <31454493+spsaroj@users.noreply.github.com>
2024-03-02 23:25:54 -0600

    navigation setup

--- cclp/src/App.tsx
+++ cclp/src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import logo from './logo.svg';
 import './App.css';
+import upload from './components/upload';
+import videoPlayer from './components/video-player';
+import searchSection from './components/search-section';
+
+import aTest from './components/A';
+import { render } from '@testing-library/react';
+import SearchSection from './components/search-section';
 
 function App() {
+  const [selectedComponent, setSelectedComponent] = useState<React.FC>(() => videoPlayer); // Set C as the initial selected component
+
+  const handleButtonClick = (component: React.FC) => {
+    setSelectedComponent(() => component);
+  };
+
+
+  const renderSelectedComponent = () => {
+    if (selectedComponent) {
+      return React.createElement(selectedComponent);
+    }
+    return null;
+  };
+  // Render the selected component
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="searchContainer" onClick={()=> handleButtonClick(SearchSection)}>
+        <img src="./cclp/public/searchImage.png" className="searchImage"/>
+        <input className="searchInput" type="text"/>
+      </div>
+
+      <div className='navbarAndContent'>
+      <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar className="sideBar" defaultValue="Explore">
+        <Menu>
+          <MenuItem onClick={()=> handleButtonClick(aTest)}> 
+            <div>Following</div>
+          </MenuItem>
+          <MenuItem> 
+            <div>Playlist</div>
+          </MenuItem>
+          <MenuItem onClick={()=> handleButtonClick(videoPlayer)}> 
+            <div>Explore</div>
+          </MenuItem>
+          <MenuItem> 
+            <div>Profile</div>
+          </MenuItem>
+          <MenuItem className='uploadVideoClass' onClick={()=> handleButtonClick(upload)}> 
+            <div>Upload Video</div>
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+      {renderSelectedComponent()}
+    </div>
+      </div>
+
     </div>
   );
 }
---
+++ cclp/src/components/A.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Atest: React.FC = () => {
+  return (
+    <div>
+      <h2>Component A</h2>
+      {/* Add additional content for Component C */}
+    </div>
+  );
+};
+
+export default Atest;
