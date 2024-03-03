ebcfed0828b915353117fd96fee63c7254bec39b
kevin harris <kcharris77@gmail.com>
2024-03-02 19:35:56 -0600

    Add function names, start work on likeButton

--- cclp/src/components/video-player.tsx
+++ cclp/src/components/video-player.tsx
@@ -1,3 +1,29 @@
+function saveToPlaylistButton(){
+    return <b/>
+}
+
+function likeButton(button_liked: boolean){
+    let icon = null
+    let no_like = <span role="img" aria-label="sheep">🐱</span>
+    let like = <span role="img" aria-label="sheep">😸</span>
+    if (button_liked){ icon = like }
+    else{ icon = no_like}
+
+    // icon should only be part of what is returned. Size of icon needs to be changed or modified in overlay.
+    return icon
+}
+
+function commentButton(){
+    return <b/>
+}
+
+function followButton(){
+    return <b/>
+}
+
+function overlay(){
+    return <b/>
+}
 export default function videoPlayer(){
     return <a></a>
 }
